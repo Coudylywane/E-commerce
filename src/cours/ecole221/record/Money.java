81dd49b77e273ce0c03333d59fb79c2743cb1420
CoudyAT <coudy@atcreative.sn>
2026-04-22 19:32:40 +0000

    feat(produit):ajout contraintes dans money

--- src/cours/ecole221/record/Money.java
+++ src/cours/ecole221/record/Money.java
@@ -1,5 +1,20 @@
 package cours.ecole221.record;
+import cours.ecole221.exeptions.InvalidCurrencyException;
+
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public record Money(BigDecimal amount, String currency) {
+    public Money {
+        Objects.requireNonNull(amount, "Amount cannot be null");
+        Objects.requireNonNull(currency, "Currency cannot be null");
+
+        if (amount.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("Amount cannot be negative");
+        }
+
+        if (!currency.equals("FCFA") && !currency.equals("EUR")) {
+            throw new InvalidCurrencyException("Currency must be FCFA or EUR");
+        }
+    }
 }
