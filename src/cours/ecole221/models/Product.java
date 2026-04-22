f143b4e03c3f3e57782bcb43eea90b7f6d17f323
CoudyAT <coudy@atcreative.sn>
2026-04-22 19:44:45 +0000

    feat(produit):ajout méthode applyDiscount dans produit

--- src/cours/ecole221/models/Product.java
+++ src/cours/ecole221/models/Product.java
@@ -3,6 +3,7 @@ package cours.ecole221.models;
 import cours.ecole221.record.Money;
 import cours.ecole221.record.SKU;
 
+import java.math.BigDecimal;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -20,5 +21,38 @@ public class Product {
         }
         this.name = name;
         this.price = Objects.requireNonNull(price, "Price cannot be null");
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public SKU getSku() {
+        return sku;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Money getPrice() {
+        return price;
+    }
+
+    public void applyDiscount(BigDecimal percentage) {
+        Objects.requireNonNull(percentage, "Percentage cannot be null");
+
+        if (percentage.compareTo(BigDecimal.valueOf(0.1)) < 0 ||
+                percentage.compareTo(BigDecimal.valueOf(100)) > 0) {
+            throw new IllegalArgumentException("Percentage must be between 0.1 and 100");
+        }
+
+        BigDecimal discount = price.amount()
+                .multiply(percentage)
+                .divide(BigDecimal.valueOf(100));
+
+        BigDecimal newAmount = price.amount().subtract(discount);
+
+        this.price = new Money(newAmount, price.currency());
     }
 }
